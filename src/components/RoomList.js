686fdee5c66d5d8dd44ce35fbf8735b414897227
Renuka <renukaalurkar@gmail.com>
2019-04-05 12:12:21 -0500

    committig again

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -16,12 +16,8 @@ class RoomList extends Component{
   }
   handleChange(e) {
    this.setState({newRoomName: e.target.value})
-   {/*in the above statement, instead of e.target.value, if you write event.target.value, you will see:unexpected use of event, no restricted globals error  */}
-  }
-  {/*in the above statement, we are setting the state of newRoomName to e.target.value.  */}
-  {/*see how we are defining the initial state of newRoomName as null, if this initial state is not set, then newRoomName undefined error occurs */}
-  {/*e.target.value is the value that is there in the textbox which is in the render method below */}
-  {/*this handleChange method is called whenever anything changes in the textbox, see in the render method below. */}
+ }
+ 
 
 
   createRoom(e){
