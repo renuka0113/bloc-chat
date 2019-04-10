78ada0d748da57edf73bcd6a3a1fe09f856daad8
Renuka <renukaalurkar@gmail.com>
2019-04-10 17:48:49 -0500

    april10

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -11,28 +11,24 @@ componentDidMount() {
        this.messagesRef.on('child_added', snapshot => {
        const message = snapshot.val();
        message.key = snapshot.key;
-      {/* console.log(message.roomId); */}
-    if(message.roomId===this.state.activeRoom.key){
        this.setState({ messages: this.state.messages.concat( message ) });
- }
+
  });
 }
 
 render(){
  return(
    <section className="messagelist">
-
-     <ul>
-        {this.state.messages.map( (message,key) =>
-          <li key={message.key}>
-            { message.content}
-          </li>
-        )}
-     </ul>
-   </section>
- );
+   <ul>
+     {this.state.messages.filter( (message) =>message.roomId==this.props.activeRoom.key))
+                         .map(  (message,key)=>
+                               <li key={message.key}>
+                                 {message.content}
+                              </li>
+                              )
+                            }
+    </ul>
 }
 }
 
-
-export default MessageList;
+export default MessageList
